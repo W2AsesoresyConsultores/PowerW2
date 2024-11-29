7675f10da23e1bd434531eef81be4aed43b265e9
Alexander0127 <AlexanderTR0127@gmail.com>
2024-11-29 13:03:34 -0500

    asd

--- src/Componentes/Empresas/Dicar/Dicar.jsx
+++ src/Componentes/Empresas/Dicar/Dicar.jsx
@@ -3,15 +3,16 @@ import Hero from './Hero'
 import FooterDicar from './FooterDicar'
 import Header from './Header'
 import Nosotros from './Nosotros'
+import Beneficios from './Beneficios'
 
 function Dicar() {
   return (
     <div>
     <Header />
     <Hero />
+    <Beneficios />
     <Nosotros />
     <FooterDicar />
-    
     </div>
   )
 }
