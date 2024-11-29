66ad19b954e25fa6f5df3e2c6db97e27f72e99c2
PieroNF <pgianmarco10@gmail.com>
2024-11-29 12:15:08 -0500

    Correciones

--- src/Componentes/Empresas/Dicar/Dicar.jsx
+++ src/Componentes/Empresas/Dicar/Dicar.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import ContainerDicar from './ContainerDicar'
 import Hero from './Hero'
-import Cultura from './Cultura'
 import FooterDicar from './FooterDicar'
 import Header from './Header'
-import About from './About'
+
 
 function Dicar() {
   return (
     <div>
     <Header />
     <Hero />
-    <Cultura />
     <FooterDicar />
     </div>
   )
