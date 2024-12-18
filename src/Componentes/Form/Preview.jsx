0647e70f242a01f95d94689ab8a3f88aed976a07
yeremiesx25 <j4r4my_2002@hotmail.com>
2024-12-18 10:22:08 -0500

    holi

--- src/Componentes/Form/Preview.jsx
+++ src/Componentes/Form/Preview.jsx
@@ -1,69 +1,144 @@
-import React from 'react';
-import { Box, Button, Typography, Grid } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import ShareButton from "../PowerAuth/ShareButton"; // Asegúrate de ajustar la ruta
+import QuestionsModal from "../PowerAuth/QuestionsModal"; // Asegúrate de ajustar la ruta
+import dayjs from "dayjs";
+import "dayjs/locale/es"; // Importar el idioma español
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Box, Button } from '@mui/material'; // Importar Box y Button de MUI
 
-const Preview = ({ step1Data, step2Data, step3Data, onConfirm, onCancel }) => {
+const Preview = ({ step1Data, step3Data, hasApplied, setIsQuestionsModalOpen, isQuestionsModalOpen, nombreReclutador, onConfirm, onCancel }) => {
+    const [jobDetails, setJobDetails] = useState([]);
+
+    useEffect(() => {
+        const details = [
+            {
+                title: "¿Por qué deberías unirte a nosotros?",
+                content: (
+                    <ul>
+                        {step3Data.beneficios.split(".").map((beneficio, index) => 
+                            beneficio.trim() && <li key={index}>{beneficio.trim()}</li>
+                        )}
+                    </ul>
+                ),
+            },
+            {
+                title: "¿Qué buscamos?",
+                content: (
+                    <ul>
+                        {step1Data.requisitos.split(".").map((requisito, index) => 
+                            requisito.trim() && <li key={index}>{requisito.trim()}</li>
+                        )}
+                    </ul>
+                ),
+            },
+            {
+                title: "¿Qué es lo que harás?",
+                content: (
+                    <ul>
+                        {step1Data.funciones.split(".").map((funcion, index) => 
+                            funcion.trim() && <li key={index}>{funcion.trim()}</li>
+                        )}
+                    </ul>
+                ),
+            },
+            {
+                title: "Horario de Trabajo",
+                content: step3Data.horario,
+            },
+        ];
+        setJobDetails(details);
+    }, [step1Data, step3Data]);
+
+    // Formatear la fecha de publicación
+    const formattedDate = dayjs(step1Data.fecha_publicacion).format("DD-MM-YYYY");
+    dayjs.extend(relativeTime);
+    dayjs.locale("es");
+    const timeAgo = dayjs(step1Data.fecha_publicacion).fromNow();
+    const capitalizedTimeAgo = timeAgo.charAt(0).toUpperCase() + timeAgo.slice(1);
+
     return (
-        <Box
-            sx={{
-                p: 3,
-                bgcolor: 'background.paper',
-                borderRadius: 2,
-                boxShadow: 3,
-                maxWidth: 600,
-                mx: 'auto',
-                my: 4,
-                overflow: 'hidden', // Evitar que se salga el contenido
-            }}
+        <div
+            className="selected-job-info w-full custom-scrollbar rounded-lg md:flex flex-col px-8 py-4 mx-8 bg-[#ffffff] hidden transition-all duration-500 font-dmsans"
+            style={{ height: "610px", overflowY: "auto" }}
         >
-            <Typography variant="h5" gutterBottom align="center">
-                Previsualización de la Oferta
-            </Typography>
+            <p className="text-gray-500 text-xs font-inter">{capitalizedTimeAgo}</p>
+            <h2 className="font-bold text-3xl text-newprimarycolor font-source">
+                {step1Data.puesto}
+            </h2>
 
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant="h6">Información General</Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Puesto: <strong>{step1Data.puesto}</strong>
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Descripción: {step1Data.descripcion}
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Requisitos: {step1Data.requisitos}
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Ubicación: {step1Data.ubicacion}
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Sueldo: {step1Data.sueldo}
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Funciones: {step1Data.funciones}
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Horario: {step3Data.horario}
-                    </Typography>
-                </Grid>
+            <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center gap-4">
+                    <div className="rounded-lg w-12 h-12">
+                        <img
+                            src={step1Data.empresa_img_url}
+                            className="w-full h-full rounded-lg"
+                            alt=""
+                        />
+                    </div>
+                    <div>
+                        <p className="text-sm font-medium text-gray-800 font-inter">
+                            {step1Data.empresa}
+                        </p>
+                        <p className="text-xs text-gray-600 font-inter">{nombreReclutador}</p>
+                    </div>
+                </div>
+            </div>
 
-                <Grid item xs={12}>
-                    <Typography variant="h6" sx={{ mt: 2 }}>Sobre el Trabajo</Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Modalidad: <strong>{step3Data.modalidad}</strong>
-                    </Typography>
-                    <Typography sx={{ wordWrap: 'break-word' }}>
-                        Beneficios: {step3Data.beneficios}
-                    </Typography>
-                </Grid>
+            <div className="flex items-center space-x-2 mb-4">
+                <span className="bg-yellowprimary text-primarycolor py-1 px-3 text-xs rounded-full font-medium tracking-wide flex items-center gap-1 font-inter">
+                    <IoLocationOutline />
+                    {step1Data.ubicacion}
+                </span>
+                <span className="bg-primarycolor text-white py-1 px-3 text-xs rounded-full font-medium tracking-wide gap-1 flex items-center font-inter">
+                    S/. {step1Data.sueldo}
+                </span>
+            </div>
 
-                <Grid item xs={12}>
-                    <Typography variant="h6" sx={{ mt: 2 }}>Preguntas para el Postulante</Typography>
-                    {Array.from({ length: 6 }).map((_, index) => (
-                        <Typography key={index} sx={{ wordWrap: 'break-word' }}>
-                            Pregunta {index + 1}: <strong>{step3Data[`preg_${index + 1}`] || 'No especificada'}</strong>
-                        </Typography>
-                    ))}
-                </Grid>
-            </Grid>
+            <div className="flex mb-4 gap-4">
+                <button
+                    className={`font-bold py-2 px-4 rounded-full w-64 font-source text-lg ${
+                        hasApplied
+                            ? "bg-yellow-200 text-primarycolor cursor-not-allowed"
+                            : "bg-[#0057c2] text-white"
+                    }`}
+                    onClick={hasApplied ? null : () => setIsQuestionsModalOpen(true)}
+                    disabled={hasApplied}
+                >
+                    {hasApplied ? "Ya has postulado" : "Postularme"}
+                </button>
+
+                <div className="">
+                    <ShareButton selectedJob={step1Data} />
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <h3 className="font-semibold text-black font-inter text-lg">Descripción</h3>
+                <p className="text-gray-800 text-base font-inter ml-2">
+                    {step1Data.descripcion}
+                </p>
+                {jobDetails.map((detail, index) => (
+                    <div key={index} className="py-2">
+                        <div className="font-semibold text-black font-inter text-lg">{detail.title}</div>
+                        <div className="mt-2 text-gray-800 text-base font-inter ml-2">{detail.content}</div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="flex justify-center mt-4">
+                <button
+                    className={`font-bold py-2 px-4 rounded-full w-64 font-source text-lg ${
+                        hasApplied
+                            ? "bg-gray-500 text-white cursor-not-allowed"
+                            : "bg-[#0057c2] text-white"
+                    }`}
+                    onClick={hasApplied ? null : () => setIsQuestionsModalOpen(true)}
+                    disabled={hasApplied}
+                >
+                    {hasApplied ? "Ya has postulado" : "Postularme"}
+                </button>
+            </div>
 
             {/* Botones de confirmación y cancelación */}
             <Box display="flex" justifyContent="space-between" mt={3}>
@@ -74,7 +149,13 @@ const Preview = ({ step1Data, step2Data, step3Data, onConfirm, onCancel }) => {
                     Confirmar y Enviar
                 </Button>
             </Box>
-        </Box>
+
+            <QuestionsModal
+                isOpen={isQuestionsModalOpen}
+                onClose={() => setIsQuestionsModalOpen(false)}
+                selectedJob={step1Data}
+            />
+        </div>
     );
 };
 
