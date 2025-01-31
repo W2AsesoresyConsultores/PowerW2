ecd43b308c30bac6e36e86a9e3e6cca4b05ed66d
yeremiesx25 <j4r4my_2002@hotmail.com>
2025-01-31 16:23:01 -0500

    a

--- src/Componentes/Empresas/Dicar/Nosotros.jsx
+++ src/Componentes/Empresas/Dicar/Nosotros.jsx
@@ -3,7 +3,7 @@ import "../../../Nosotros.css";
 
 const Nosotros = () => {
   return (
-    <div id="nosotros" className="bg-white py-16 px-4 md:px-16">
+    <div id="nosotros" className="bg-white py-16 px-4 md:px-4">
       {/* Título general */}
       <div className="text-center mb-12">
         <h1 className="font-source font-bold text-5xl text-[#2f4daa] text-center">
@@ -13,10 +13,10 @@ const Nosotros = () => {
 
       {/* Contenedor del video manteniendo diseño */}
       <div className="relative w-full flex justify-center">
-        <div className="player-wrapper rounded-lg overflow-hidden shadow-lg w-full max-w-3xl">
+        <div className="player-wrapper rounded-lg overflow-hidden shadow-lg w-3/4 max-w-full" style={{ height: '500px' }}>
           <iframe
             src="https://drive.google.com/file/d/1nB3ak8uh0EhcuJR49sLPHUFinL1nyINj/preview"
-            className="w-full aspect-video"
+            className="w-full h-full"
             allow="autoplay"
           ></iframe>
         </div>
